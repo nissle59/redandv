c1b9431558ee1f70949e363333757dd9344117c3
nissle59 <nixncom@gmail.com>
2024-01-18 17:05:59 +0300

    Рабочая версия 1.0

--- main.py
+++ main.py
@@ -1,6 +1,96 @@
+import json
+
 import requests
 import csv
 from pathlib import Path
+import threading
+
+cats_handler = open('categories.txt', 'w')
+
+
+def pretty(dd, key_f, indent=0):
+    global cats_handler
+    if isinstance(dd, list):
+        for d in dd:
+            for key, value in d.items():
+                # print('\t' * indent + str(key))
+                if isinstance(value, dict) or isinstance(value, list):
+                    pretty(value, key_f, indent + 1)
+                else:
+                    if key == key_f:
+                        cats_handler.write('\t' * (indent + 1) + f'({d.get("id")}) ' + str(value) + '\n')
+    else:
+        d = dd
+        for key, value in d.items():
+            # print('\t' * indent + str(key))
+            if isinstance(value, dict) or isinstance(value, list):
+                pretty(value, key_f, indent + 1)
+            else:
+                if key == key_f:
+                    cats_handler.write('\t' * (indent + 1) + f'({d.get("id")}) ' + str(value) + '\n')
+
+
+def create_tree(data):
+    tree = {}
+    nodes = {}
+    for i in data:
+        id = i['id']
+        nodes[id] = i
+
+    forest = []
+    for i in data:
+        id = i['id']
+        parent_id = i.get('parent_id', None)
+        node = nodes[id]
+
+        # either make the node a new tree or link it to its parent
+        if (id == parent_id) or (parent_id == None):
+            # start a new tree in the forest
+            forest.append(node)
+            forest.sort(key=lambda x: (x['id']))
+        else:
+            # add new_node as child to parent
+            parent = nodes[parent_id]
+            if not 'children' in parent:
+                # ensure parent has a 'children' field
+                parent['children'] = []
+            children = parent['children']
+            children.append(node)
+            children.sort(key=lambda x: (x['id']))
+    return forest
+
+
+def find_in_nested_dict(d: dict, value, key='id'):
+    if isinstance(d, list):
+        for dd in d:
+            if key in dd:
+                if dd[key] == value:
+                    return dd
+            for k, v in dd.items():
+                if isinstance(v, dict):
+                    result = find_in_nested_dict(v, value)
+                    if result is not None:
+                        return result
+                elif isinstance(v, list):
+                    for i in v:
+                        result = find_in_nested_dict(v, value)
+                        if result is not None:
+                            return result
+    else:
+        if key in d:
+            if d[key] == value:
+                return d
+        for k, v in d.items():
+            if isinstance(v, dict):
+                result = find_in_nested_dict(v, value)
+                if result is not None:
+                    return result
+            elif isinstance(v, list):
+                for i in v:
+                    result = find_in_nested_dict(v, value)
+                    if result is not None:
+                        return result
+    return None
 
 
 class Redan():
@@ -9,38 +99,58 @@ class Redan():
     cat_url = f'{base_url}/category/?key={token}'
     prod_url = f'{base_url}/product/?op=stock&key={token}'
     img_url = f'{base_url}/image/?op=item&key={token}&id='
+    base_img_path = 'images/'
 
     categories = {}
     cat_to_load = []
     state = -1
     products = []
+    ctl = []
 
     def __init__(self):
         self.state = self.init_categories()
+
+    def collect_cat_ids(self, root_cats: list):
+        def rotate(cats):
+            if isinstance(cats, list):
+                for cat in cats:
+                    children = cat.get('children', None)
+                    if children:
+                        self.ctl.append(cat.get('id', ''))
+                        rotate(children)
+                    else:
+                        self.ctl.append(cat.get('id', ''))
+            else:
+                cat = cats
+                children = cat.get('children', None)
+                if children:
+                    self.ctl.append(cat.get('id', ''))
+                    rotate(children)
+                else:
+                    self.ctl.append(cat.get('id', ''))
+
+        self.ctl = []
+        rotate(root_cats)
+        print(f'{len(self.ctl)} categories added')
 
     def init_categories(self):
         r = requests.get(self.cat_url)
         try:
             js = r.json()['categories']
-            cat_filter = Path("cat_filter.csv")
             self.cat_to_load = []
-            if not (cat_filter.is_file()):
-                with open("cat_filter.csv", mode="w", encoding='utf-8-sig') as w_file:
-                    file_writer = csv.writer(w_file, delimiter=";", lineterminator="\r")
-                    file_writer.writerow(["id", "Category","Parent", "Get"])
-                    for item in js:
-                        file_writer.writerow([item['id'], item['name'], item.get('parent_id',''), "1"])
-                        self.cat_to_load.append(item['id'])
-                return 1
-            else:
-                with open("cat_filter.csv", mode="r", encoding='utf-8-sig') as r_file:
-                    file_reader = csv.DictReader(r_file, delimiter=";")
-                    for row in file_reader:
-                        if row['Get'] == '1':
-                            self.cat_to_load.append(row['id'])
-                return 0
+            self.js_cats = create_tree(js)
+            pretty(self.js_cats, 'name')
+            cats_handler.close()
+            self.cat_to_load = []
+            with open('categories_to_parse.txt', 'r') as f:
+                input_cats = f.read().split('\n')
+            for i in input_cats:
+                self.cat_to_load.append(find_in_nested_dict(self.js_cats, i))
+            self.collect_cat_ids(self.cat_to_load)
+            return 1
 
-        except:
+        except Exception as e:
+            print(e)
             return -1
 
     def get_products(self):
@@ -48,13 +158,13 @@ class Redan():
         try:
             js = r.json()['stock']
             for prod in js:
-                if prod.get('category_id') in self.cat_to_load:
+                if prod.get('category_id') in self.ctl:
                     self.products.append(prod)
             return 1
         except:
             return -1
 
-    def make_csvs(self,msk = 'msk.csv', chab = 'chab.csv'):
+    def make_csvs(self, msk='msk.csv', chab='chab.csv'):
         mskf = open(msk, mode="w", encoding='utf-8-sig')
         chabf = open(chab, mode="w", encoding='utf-8-sig')
         names = [
@@ -71,7 +181,7 @@ class Redan():
             'Авиадоставка',
             'Товар в пути'
         ]
-        msk_writer = csv.DictWriter(mskf, delimiter = ";",lineterminator="\r", fieldnames=names)
+        msk_writer = csv.DictWriter(mskf, delimiter=";", lineterminator="\r", fieldnames=names)
         chab_writer = csv.DictWriter(chabf, delimiter=";", lineterminator="\r", fieldnames=names)
         msk_writer.writeheader()
         chab_writer.writeheader()
@@ -96,18 +206,18 @@ class Redan():
                     price = pr['price']
 
             msk_writer.writerow({
-                'Артикул(1)':product.get('sku'),
-                'Наименование(2)':product.get('name'),
-                'Бренд(3)':product.get('brand'),
-                'Склад':'Москва',
-                'Количество(5)':msk_count,
-                'Цена(6)':price,
-                'Распродажа':'',
-                'Мин. заказ':'1',
-                'Срок поставки (Рабочие дни)':'0',
-                'Уцененный товар':'',
-                'Авиадоставка':'',
-                'Товар в пути':''
+                'Артикул(1)': product.get('sku'),
+                'Наименование(2)': product.get('name'),
+                'Бренд(3)': product.get('brand'),
+                'Склад': 'Москва',
+                'Количество(5)': msk_count,
+                'Цена(6)': price,
+                'Распродажа': '',
+                'Мин. заказ': '1',
+                'Срок поставки (Рабочие дни)': '0',
+                'Уцененный товар': '',
+                'Авиадоставка': '',
+                'Товар в пути': ''
             })
             chab_writer.writerow({
                 'Артикул(1)': product.get('sku'),
@@ -126,8 +236,37 @@ class Redan():
         mskf.close()
         chabf.close()
 
+    def get_images(self):
+        for product in self.products:
+            if product.get('category_id') in self.ctl:
+                try:
+                    r = requests.get(f'{self.base_url}/image/?op=item&id={product.get("id")}&key={self.token}')
+                    b = r.content
+                    try:
+                        err = r.json()['error']
+                        print(f'for {product.get("id")} : {err}')
+                        continue
+                    except:
+                        pass
+                    # print(b)
+                    if len(b) > 0:
+                        if b[:6] != b'<html>':
+                            print(f'Load image for {product["id"]}')
+                            with open(self.base_img_path + product.get('sku') + '@' + product.get('id') + '.jpg',
+                                      'wb') as f:
+                                f.write(b)
+                except Exception as e:
+                    print(e)
+
+
 if __name__ == '__main__':
     rdv = Redan()
     if rdv.state > -1:
-        if rdv.get_products()>0:
+        if rdv.get_products() > 0:
+            skulist = []
+            for product in rdv.products:
+                skulist.append(product.get('sku') + '@' + product.get('id'))
+            newlist = list(set(skulist))
+            print(f'{len(skulist)} -> {len(newlist)}')
             rdv.make_csvs()
+            rdv.get_images()
